157216710f54a2d9ca0361ed47012914776c85cb
majidghol <giba.gibson87@gmail.com>
2026-06-08 03:07:14 +0330

    Fix PWA + Service Worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,13 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker installed");
+self.addEventListener("install", event => {
+    console.log("Service Worker installed");
+    self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  // ساده نگه می‌داریم
+self.addEventListener("activate", event => {
+    console.log("Service Worker activated");
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(fetch(event.request));
 });
